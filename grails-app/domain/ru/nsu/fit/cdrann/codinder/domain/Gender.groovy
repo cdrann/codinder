b7733ebf405d904bb2c686ee61da3aecfaf2eddc
cdrann <a.redko@g.nsu.ru>
2021-04-14 01:14:32 +0700

    commit

--- grails-app/domain/ru/nsu/fit/cdrann/codinder/domain/Gender.groovy
+++ grails-app/domain/ru/nsu/fit/cdrann/codinder/domain/Gender.groovy
@@ -5,7 +5,7 @@ class Gender {
     String name
 
     static constraints = {
-        name blank: false, size: 2..10, unique: true
+        name blank: false, size: 1..10, unique: true
     }
 
     @Override
